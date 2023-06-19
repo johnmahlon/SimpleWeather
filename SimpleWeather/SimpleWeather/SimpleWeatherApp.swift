bd3cf40703a2e1d55d53c3e1eda7fc239ae1f729
John Peden <john.peden@protonmail.com>
2023-06-19 16:16:50 -0500

    initial commit

---
+++ SimpleWeather/SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by John Peden on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
